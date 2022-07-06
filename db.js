f11040ca425d40fa9f2b8669804332722e9ef7ee
Youssef Afify <youssef,afify122@gmail.com>
2022-07-07 01:32:59 +0200

    make submit function

--- db.js
+++ db.js
@@ -1,14 +1,14 @@
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database('db.sqlite');
 
-function addFeedback(name, email, message) {
+module.exports.addFeedback = function (name, email, message) {
     db.run(`
         INSERT INTO feedbacks (name, email, message)
         VALUES (?, ?, ?)
     `, [name, email, message]);
 }
 
-function getFeedbacks() {
+module.exports. getFeedbacks = function () {
     return new Promise((resolve, reject) => {
         db.all(`
             SELECT * FROM feedbacks
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
+const { route } = require('../app');
 var router = express.Router();
-
+var db = require('../db');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/submit', function(req, res, next) {
+  res.render('submit', { title: 'Express' });
+});
+
+
+
+router.post('/feedback', function(req, res, next) {
+  // store feedback 
+  db.addFeedback(req.body.name, req.body.email, req.body.message);
+  res.redirect('/submit');
+})
+
 
 module.exports = router;
