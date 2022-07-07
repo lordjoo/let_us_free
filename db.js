6d14f5b861f56f98d02d1bfc388ffc662bbcefee
Youssef Afify <youssef.afify122@gmail.com>
2022-07-07 02:10:18 +0200

    try with ahmed

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const sqlite3 = require('sqlite3');
-const db = new sqlite3.Database(':memory:');
+const db = new sqlite3.Database('db.sqlite');
 
 module.exports.addFeedback = function (name, email, message) {
     db.run(`
